a93025a9e9ac18ecb55e92e3eef26fd5e64cf4eb
iCharly666 <soportecarlos07@gmail.com>
2023-07-11 17:22:47 -0600

    login and logout update header option

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [ 
-    path('', views.index, name='index'),
-    path('about/', views.about),
-    #path('hello/<str:username>', views.hello),
-    path('projects/', views.projects),
-    path('tasks/', views.tasks),
-    path('register/', views.register, name='register'),  # Utiliza el nombre de función actualizado
-    #path('login/', views.login_view, name='login'),  # Utiliza el nombre de función actualizado
-    path('login/', views.login_view, name='login'),  # Utiliza el nombre de función actualizado
-    path('logout/', views.logout_view, name='logout')
-
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("about/", views.about),
+    # path('hello/<str:username>', views.hello),
+    path("projects/", views.projects),
+    path("tasks/", views.tasks),
+    path(
+        "register/", views.register, name="register"
+    ),  # Utiliza el nombre de función actualizado
+    # path('login/', views.login_view, name='login'),  # Utiliza el nombre de función actualizado
+    path(
+        "login/", views.login_view, name="login"
+    ),  # Utiliza el nombre de función actualizado
+    path("logout/", views.logout_view, name="logout"),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,73 +1,78 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-#from blog.forms.forms import CustomLoginForm
+
+# from blog.forms.forms import CustomLoginForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponse
-#from django.contrib.auth.forms import AuthenticationForm
+
+# from django.contrib.auth.forms import AuthenticationForm
 from blog.models import Project
+
 # Importar clase de formulario personalizado desde la carpeta forms - forms.py
 from blog.forms.forms import CustomUserCreationForm
 
 import logging
+
 logger = logging.getLogger(__name__)
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, "about.html")
+
 
 def hello(request, username):
     print(username)
     return HttpResponse("Hello %s" % username)
 
+
 def projects(request):
     projects = list(Project.objects.values())
-    return render(request, 'projects.html')
+    return render(request, "projects.html")
+
 
 def tasks(request):
-    return render(request, 'tasks.html')
-
+    return render(request, "tasks.html")
 
 
 # Función para registrar un usuario mediante formulario y dirigirlo al login una vez se registraron sus datos correctamente.
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')
+            return redirect("login")
     else:
         form = CustomUserCreationForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, "register.html", {"form": form})
+
 
 # login
 def login_view(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            request.session['username'] = user.username  # Guarda el nombre de usuario en la sesión
-            return redirect('/')  # Redirige a la página principal después del inicio de sesión exitoso
+            request.session[
+                "username"
+            ] = user.username  # Guarda el nombre de usuario en la sesión
+            return redirect(
+                "/"
+            )  # Redirige a la página principal después del inicio de sesión exitoso
     else:
         form = AuthenticationForm()
-    
-    return render(request, 'login.html', {'form': form})
+
+    return render(request, "login.html", {"form": form})
 
 
 # logout
 from django.contrib.auth import logout
 
+
 def logout_view(request):
     logout(request)
-    return redirect('index')  # Redirige a la página principal después de cerrar sesión
-
-
-
-
-
-
-
+    return redirect("index")  # Redirige a la página principal después de cerrar sesión
