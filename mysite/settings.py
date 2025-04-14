4048cf2cd649689f8a22e9451513e59a459f1920
iCharly666 <soportecarlos07@gmail.com>
2025-04-14 17:53:42 -0600

    Eliminando archivos innecesarios del proyecto

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3_yjn)+&kly_#(8(6qz@!3uglg^wn4bgkx$oxa2u+tu5l=osnc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ccalderon.pythonanywhere.com']
 
